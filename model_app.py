c8bd356b4a8b34f6e00f124dbfe8c32b64fcd30d
prashant-more-datascience <pm257352@gmail.com>
2025-06-09 18:51:05 +0530

    Update model_app.py

--- model_app.py
+++ model_app.py
@@ -282,7 +282,7 @@ if st.button("PREDICT"):
         input_data = np.array([[cylinders, displacement, weight, horsepower, acceleration] + fuel_encoded])
         input_scaled = scaler.transform(input_data)
        # XGBoost Prediction
-        st.session_state.kmpl_prediction = (xgb_model.predict(input_scaled)[0])+1.5  
+        st.session_state.kmpl_prediction = (xgb_model.predict(input_scaled)[0])+1.7  
 
         # Get LLM suggestions
         llm_response = suggest_car_modifications(
